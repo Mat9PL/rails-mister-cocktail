2f02a1f9d79d855c35eabfea90000057e7c1d74e
Mateusz Brzezanski <mateusz.brzezanski@gmail.com>
2020-05-21 16:01:11 +0200

    bugs fixed

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -9,7 +9,7 @@ class DosesController < ApplicationController
     @dose = Dose.create(dose_params)
     @dose.cocktail = @cocktail
     if @dose.valid?
-      @cocktail.save
+      @dose.save
       redirect_to cocktail_path(@cocktail)
     else
       render :new
